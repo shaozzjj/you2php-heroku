eb293d8b6e8362ee8b21e8d0b814de61353ba21f
shaozzjj <38802356+shaozzjj@users.noreply.github.com>
2018-04-28 16:28:23 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
+define('APIKEY', 'AIzaSyCt1Rk35JGQ9NRY2X2S5ZtjRGCBkdqaV_c');//这里改成您的api key
+define('GJ_CODE', 'tw');//国家代码
 define('SITE_NAME', 'Youtube');//网站名字
 define('TITLENAME', 'Youtube');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
